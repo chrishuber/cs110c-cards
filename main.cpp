45fb70e2886b0a04d2fd028a762c82ee0b8c9484
Chris Huber <chris_sf@hotmail.com>
2020-01-26 07:54:29 -0800

    Adding sample output to end of main.cpp

--- main.cpp
+++ main.cpp
@@ -69,3 +69,47 @@ int main() {
     theGame.deck()->shuffleDeck();
     theGame.deck()->displayDeck();
 }
+
+/* SAMPLE OUTPUT
+ -------- INIT COMPLETE --------
+ -------- CURRENT DECK --------
+ 2D 3D 4D 5D 6D 7D 8D 9D TD JD QD KD AD 2C 3C 4C 5C 6C 7C 8C 9C TC JC QC KC AC 2H 3H 4H 5H 6H 7H 8H 9H TH JH QH KH AH 2S 3S 4S 5S 6S 7S 8S 9S TS JS QS KS AS
+ -------- SHUFFLED --------
+ -------- CURRENT DECK --------
+ TH JS 3H 2D 9H 8H JH 7S 5D 8S 4S 4D JC QC 8D AH 2H TS 6S 7C 6C 8C 5C 2C QH AD JD 9S 4H 9D QS TD 3D 3C 4C 9C TC KC 5H AC 7H 2S KH KS QD KD AS 5S 3S 6H 7D 6D
+ Afsarah has joined High Card
+ Blodwyn has joined High Card
+ Cadence has joined High Card
+ Dulcinea has joined High Card
+ Round 1 has begun!
+ Dealing cards...
+ Players anteing...
+ Current pot is 4
+ Afsarah's Hand: HT SJ H3 D2 H9
+ Blodwyn's Hand: H8 HJ S7 D5 S8
+ Cadence's Hand: S4 D4 CJ CQ D8
+ Dulcinea's Hand: HA H2 ST S6 C7
+ Afsarah plays SJ
+ Afsarah bets 20 chips
+ The pot is now 24
+ Blodwyn plays S7
+ Blodwyn plays D5
+ Blodwyn bets 15 chips
+ The pot is now 39
+ Dulcinea plays HA
+ Blodwyn plays S8
+ Afsarah draws a card
+ Afsarah draws a card
+ Cadence draws a card
+ Dulcinea draws a card
+ Afsarah's Hand: HT H3 D2 H9 C6 C8
+ Blodwyn's Hand: H8 HJ
+ Cadence's Hand: S4 D4 CJ CQ D8 C5
+ Dulcinea's Hand: H2 ST S6 C7 C2
+ -------- CURRENT DECK --------
+ QH AD JD 9S 4H 9D QS TD 3D 3C 4C 9C TC KC 5H AC 7H 2S KH KS QD KD AS 5S 3S 6H 7D 6D
+ -------- SHUFFLED --------
+ -------- CURRENT DECK --------
+ 4C 6D TD QH 3C 9S KC 4H TC 7D 3S KD QS 7H 2S KH QD AC 6H 5S 5H JD AD KS AS 9C 3D 9D
+ Program ended with exit code: 0
+ */
